37782fc409a1277d37526e54d41fdefc717d0961
Achmad Syifau Syafawi <achmadsyifausyafawi@Achmads-MacBook-Pro-2.local>
2024-05-11 23:33:38 +0700

    add image access url

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const key = process.env.NEXT_PUBLIC_API_URL;
-const protocol = "http";
-const hostname = "localhost";
+let protocol = "http";
+let hostname = "localhost";
 
 if (key != null) {
-  const splitData = key.split(":");
+  let splitData = key.split(":");
   protocol = splitData[0];
   hostname = splitData[1].slice(2);
 }
