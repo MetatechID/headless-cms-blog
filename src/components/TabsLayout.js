b6a28558f39df54e26226e758af7b650e53c1b11
Achmad Syifau Syafawi <achmadsyifausyafawi@Achmads-MacBook-Pro-2.local>
2024-05-22 02:22:40 +0700

    [style] change break point on program content layout

--- src/components/TabsLayout.js
+++ src/components/TabsLayout.js
@@ -3,7 +3,7 @@ import { Tabs } from "@/components/ui/tabs";
 
 export function TabsLayout({ itemTabs }) {
   return (
-    <div className="relative mx-auto my-10 flex h-screen w-full max-w-6xl flex-col items-start justify-start  [perspective:1000px] sm:flex-row md:h-[40rem]">
+    <div className="relative mx-auto my-10 flex h-screen w-full max-w-6xl flex-col items-start justify-start [perspective:1000px]  sm:flex-row sm:px-0 md:h-[40rem] md:px-10">
       <Tabs tabs={itemTabs} />
     </div>
   );
